5a0eeba1240df35449dbd143a33e85a9d6db966e
erhgp45th45tgpw <138838405+NathanMartin1@users.noreply.github.com>
2024-01-27 15:09:31 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+//https://teachablemachine.withgoogle.com/models/4l8cJ3t1U/model.json
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: "png",
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+ data_uri +'"/>';
+    })
+}
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/9_aVMMP8q/model.json',modelLoaded);
+function modelLoaded(){
+    window.alert('The ml5 model has successfully loaded!');
+}
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify( img, gotResult);
+}
+function gotResult(error,results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("nameOfResult").innerHTML = results[0].label;
+        document.getElementById("accuracyOfResult").innerHTML = (results[0].confidence*100).toFixed(3);
+}
+}
